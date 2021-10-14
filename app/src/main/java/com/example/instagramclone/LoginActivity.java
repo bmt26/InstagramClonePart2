d3d5ce214fff9d84b1dad1e17599af86be29b031
Brian Temple <bmt26@njit.edu>
2021-10-14 17:16:21 -0400

    Initial commit

---
+++ app/src/main/java/com/example/instagramclone/LoginActivity.java
@@ -0,0 +1,42 @@
+package com.example.instagramclone;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class LoginActivity extends AppCompatActivity {
+
+    public static final String TAG = "LoginActivity";
+    private EditText etUsername;
+    private EditText etPassword;
+    private Button btnLogin;
+
+    @Override
+    protected void onCreate(Bundle InstanceState) {
+        super.onCreate(InstanceState);
+        setContentView(R.layout.activity_login);
+
+        etUsername = findViewById(R.id.etUsername);
+        etPassword = findViewById(R.id.etPassword);
+        btnLogin = findViewById(R.id.btnLogin);
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.i(TAG, "onClick login button");
+                String username = etUsername.getText().toString();
+                String password = etPassword.getText().toString();
+                loginUser(username, password);
+            }
+        });
+
+    }
+
+    private void loginUser(String username, String password) {
+        Log.i(TAG, "Attempting to login user " + username);
+        // TODO: navigate to the main activity if the user has signed in properly
+    }
+}
