536fac452d74c37afae761ff17e8255f37b1e20d
Brian <bmt26@njit.edu>
2021-10-15 20:01:05 -0400

    Jquery Request

--- app/src/main/java/com/example/instagramclone/ParseApplication.java
+++ app/src/main/java/com/example/instagramclone/ParseApplication.java
@@ -13,6 +13,9 @@ public class ParseApplication extends Application {
         super.onCreate();
 
         // Register your parse models
+        ParseObject.registerSubclass(Post.class);
+
+        // Register your parse models
         Parse.initialize(new Parse.Configuration.Builder(this)
                 .applicationId("MW6IoZAUydcWRwQZ60GLf7vBpD7kB34hvngpSNBd")
                 .clientKey("FQKpX2O0eUlUk7LDlhDv2rD253hZhShL3nUk4jAU")
