9a056707ee8712f5a7641e7ca2dec48a50745dd5
bmt26 <bmt26@njit.edu>
2021-10-19 13:26:15 -0400

    All fragments can be displayed by clicking on the various bottom navigation buttons

--- app/src/main/java/com/example/instagramclone/fragments/PostsFragment.java
+++ app/src/main/java/com/example/instagramclone/fragments/PostsFragment.java
@@ -2,7 +2,10 @@ package com.example.instagramclone.fragments;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
@@ -15,6 +18,8 @@ import com.example.instagramclone.R;
  */
 public class PostsFragment extends Fragment {
 
+    private RecyclerView rvPosts;
+
     public PostsFragment() {
         // Required empty public constructor
     }
@@ -24,5 +29,18 @@ public class PostsFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_posts, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        rvPosts = view.findViewById(R.id.rvPosts);
+
+        // Steps to use the recycler view:
+        // 0. create layout for one row in the list
+        // 1. create the adapter
+        // 2. create the data source
+        // 3. set the adapter on the recycler view
+        // 4. set the layout manager on the recycler view
     }
 }
